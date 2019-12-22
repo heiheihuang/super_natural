593ed51333cdc55be13a54b3ac2f848cdd48744a
heiheihuang <1245026448@qq.com>
2019-12-22 22:35:47 +0800

    增加获取IP地址工具类

--- 001-校园快递/代码管理/express-master/src/main/java/com/example/express/Application.java
+++ 001-校园快递/代码管理/express-master/src/main/java/com/example/express/Application.java
@@ -1,19 +1,27 @@
 package com.example.express;
 
+import com.example.express.common.util.IPUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext application = SpringApplication.run(Application.class, args);
+        Environment env = application.getEnvironment();
+        String ip = IPUtil.getServerIp();
+        String port = env.getProperty("server.port");
         log.error(
                 "\n\n-------------------------------- " +
                         "Express application started successfully!!!" +
-                        " --------------------------------\n\n");
+                        " --------------------------------\n\n" +
+                        "Local: \t\thttp://localhost:" + port + "/\n" +
+                        "External: \thttp://" + ip + ":" + port + "/\n");
     }
 
 }
